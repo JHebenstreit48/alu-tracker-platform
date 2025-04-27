6efb4c8a4c3307d2c3752ac8f058118945187492
Justin <j.hebenstreit25@gmail.com>
2025-04-27 01:09:16 -0500

    removed map due to issues

--- client/src/ManufacturersMap/BrandInfo/BrandQuickList.tsx
+++ client/src/ManufacturersMap/BrandInfo/BrandQuickList.tsx
@@ -1,43 +1,72 @@
 import { Link } from "react-router-dom";
 import "@/SCSS/Brands/BrandQuickList.scss";
+import BackToTop from "@/components/BackToTopButton";
 
 interface BrandQuickListProps {
   manufacturers: {
     _id: string;
     brand: string;
     slug: string;
+    country: string;
   }[];
 }
 
 export default function BrandQuickList({ manufacturers }: BrandQuickListProps) {
-  // Group manufacturers alphabetically
-  const groupedManufacturers = manufacturers.reduce((acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
-    const firstLetter = manufacturer.brand.charAt(0).toUpperCase();
-    if (!acc[firstLetter]) {
-      acc[firstLetter] = [];
+  // Group manufacturers by Country first
+  const groupedByCountry = manufacturers.reduce((acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
+    const country = manufacturer.country || "Unknown";
+    if (!acc[country]) {
+      acc[country] = [];
     }
-    acc[firstLetter].push(manufacturer);
+    acc[country].push(manufacturer);
     return acc;
   }, {});
 
-  const sortedLetters = Object.keys(groupedManufacturers).sort();
+  // Sort countries alphabetically
+  const sortedCountries = Object.keys(groupedByCountry).sort();
 
   return (
     <div className="brand-quick-list">
-      {sortedLetters.map((letter) => (
-        <div key={letter} className="brand-letter-section">
-          <h3>{letter}</h3>
-          <ul>
-            {groupedManufacturers[letter].map((manufacturer) => (
-              <li key={manufacturer._id}>
-                <Link to={`/manufacturers/${manufacturer.slug}`}>
-                  {manufacturer.brand}
-                </Link>
-              </li>
+      {sortedCountries.map((country) => {
+        const countryManufacturers = groupedByCountry[country];
+
+        // Group manufacturers in each country by first letter
+        const groupedByLetter = countryManufacturers.reduce((acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
+          const firstLetter = manufacturer.brand.charAt(0).toUpperCase();
+          if (!acc[firstLetter]) {
+            acc[firstLetter] = [];
+          }
+          acc[firstLetter].push(manufacturer);
+          return acc;
+        }, {});
+
+        const sortedLetters = Object.keys(groupedByLetter).sort();
+
+        return (
+          <div key={country} className="country-section">
+            <h2 className="country-header">{country}</h2> {/* ✅ Country visibly shown */}
+
+            {sortedLetters.map((letter) => (
+              <div key={letter} className="brand-letter-section">
+                <h3>{letter}</h3> {/* ✅ First letter header */}
+                <ul>
+                  {groupedByLetter[letter]
+                    .sort((a, b) => a.brand.localeCompare(b.brand))
+                    .map((manufacturer) => (
+                      <li key={manufacturer._id}>
+                        <Link to={`/manufacturers/${manufacturer.slug}`}>
+                          {manufacturer.brand}
+                        </Link>
+                        
+                      </li>
+                    ))}
+                </ul>
+                <BackToTop />
+              </div>
             ))}
-          </ul>
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
