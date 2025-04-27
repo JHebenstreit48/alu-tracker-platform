706ae799b2740d4ba697033ed7b08da47a6d98c7
Justin <j.hebenstreit25@gmail.com>
2025-04-27 10:20:32 -0500

    added more brand entries and updated files to make country a string array

--- client/src/Brands/BrandInfo/BrandQuickList.tsx
+++ client/src/Brands/BrandInfo/BrandQuickList.tsx
@@ -7,20 +7,27 @@ interface BrandQuickListProps {
     _id: string;
     brand: string;
     slug: string;
-    country: string;
+    country: string[]; // ✅ Now an array
   }[];
 }
 
 export default function BrandQuickList({ manufacturers }: BrandQuickListProps) {
   // Group manufacturers by Country first
-  const groupedByCountry = manufacturers.reduce((acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
-    const country = manufacturer.country || "Unknown";
-    if (!acc[country]) {
-      acc[country] = [];
-    }
-    acc[country].push(manufacturer);
-    return acc;
-  }, {});
+  const groupedByCountry = manufacturers.reduce(
+    (acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
+      const countries = manufacturer.country.length > 0 ? manufacturer.country : ["Unknown"];
+
+      countries.forEach((country) => {
+        if (!acc[country]) {
+          acc[country] = [];
+        }
+        acc[country].push(manufacturer);
+      });
+
+      return acc;
+    },
+    {}
+  );
 
   // Sort countries alphabetically
   const sortedCountries = Object.keys(groupedByCountry).sort();
@@ -31,14 +38,17 @@ export default function BrandQuickList({ manufacturers }: BrandQuickListProps) {
         const countryManufacturers = groupedByCountry[country];
 
         // Group manufacturers in each country by first letter
-        const groupedByLetter = countryManufacturers.reduce((acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
-          const firstLetter = manufacturer.brand.charAt(0).toUpperCase();
-          if (!acc[firstLetter]) {
-            acc[firstLetter] = [];
-          }
-          acc[firstLetter].push(manufacturer);
-          return acc;
-        }, {});
+        const groupedByLetter = countryManufacturers.reduce(
+          (acc: Record<string, { _id: string; brand: string; slug: string; }[]>, manufacturer) => {
+            const firstLetter = manufacturer.brand.charAt(0).toUpperCase();
+            if (!acc[firstLetter]) {
+              acc[firstLetter] = [];
+            }
+            acc[firstLetter].push(manufacturer);
+            return acc;
+          },
+          {}
+        );
 
         const sortedLetters = Object.keys(groupedByLetter).sort();
 
@@ -57,11 +67,9 @@ export default function BrandQuickList({ manufacturers }: BrandQuickListProps) {
                         <Link to={`/manufacturers/${manufacturer.slug}`}>
                           {manufacturer.brand}
                         </Link>
-                        
                       </li>
                     ))}
                 </ul>
-  
               </div>
             ))}
             <BackToTop />
