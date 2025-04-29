9186dd2ea6ad6f401cb3bb4adf5f18eb06537a18
Justin <j.hebenstreit25@gmail.com>
2025-04-29 13:35:58 -0500

    near the very end of finishing updating the back end photos and .json files

--- client/src/assets/images/images.ts
+++ client/src/assets/images/images.ts
@@ -1,29 +1,5 @@
 import StarIcon from "@/assets/star-icon/star-icon.png";
 
-import PragaR1 from "@/assets/car-photos/P/Praga/praga-r1.jpg";
-import PuritaliaBerlinetta from "@/assets/car-photos/P/Puritalia/puritalia-berlinetta.jpg";
-import RaesrTachyonSpeed from "@/assets/car-photos/R/Raesr/raesr-tachyon-speed.jpg";
-import RaesrTartarus from "@/assets/car-photos/R/Raesr/raesr-tartarus.jpg";
-import RenaultDezir from "@/assets/car-photos/R/Renault/renault-dezir.jpg";
-import RenaultRS01 from "@/assets/car-photos/R/Renault/renault-rs-01.jpg";
-import RenaultTrezor from "@/assets/car-photos/R/Renault/renault-trezor.jpg";
-import RezvaniBeastX from "@/assets/car-photos/R/Rezvani/rezvani-beast-x.jpg";
-import RimacConceptS from "@/assets/car-photos/R/Rimac/rimac-concept-s.jpg";
-import RimacNevera from "@/assets/car-photos/R/Rimac/rimac-nevera.jpg";
-import RimacNeveraTimeAttack from "@/assets/car-photos/R/Rimac/rimac-nevera-time-attack.jpg";
-import SaleenS1 from "@/assets/car-photos/S/Saleen/saleen-s1.jpg";
-import SaleenS7TwinTurbo from "@/assets/car-photos/S/Saleen/saleen-s7-twin-turbo.jpg";
-import SinR1550 from "@/assets/car-photos/S/SIN/sin-r1-550.jpg";
-import SpaniaGTA2015GTASpano from "@/assets/car-photos/S/Spania/spania-gta-2015-gta-spano.jpg";
-import SSCTuatara from "@/assets/car-photos/S/SSC/ssc-tuatara.jpg";
-import TechrulesAT96TrackVersion from "@/assets/car-photos/T/Techrules/techrules-at96-track-version.jpg";
-import TorinoDesignSuperSport from "@/assets/car-photos/T/TorinoDesign/torino-design-super-sport.jpg";
-import Toroidion1MW from "@/assets/car-photos/T/Toroidion/toroidion-1mw.jpg";
-import TrionNemesis from "@/assets/car-photos/T/Trion/trion-nemesis.jpg";
-import TushekTS900RacerPro from "@/assets/car-photos/T/Tushek/tushek-ts-900-racer-pro.jpg";
-import TVRGriffith from "@/assets/car-photos/T/TVR/tvr-griffith.jpg";
-import TVRSagaris from "@/assets/car-photos/T/TVR/tvr-sagaris.jpg";
-import UltimaRS from "@/assets/car-photos/U/Ultima/ultima-rs.jpg";
 import VencerSarthe from "@/assets/car-photos/V/Vencer/vencer-sarthe.jpg";
 import Vision1789 from "@/assets/car-photos/V/Vision/vision-1789.jpg";
 import VLFForce1V10 from "@/assets/car-photos/V/VLF/vlf-force-1-v10.jpg";
@@ -40,30 +16,6 @@ import ZenvoTSRS from "@/assets/car-photos/Z/Zenvo/zenvo-tsr-s.jpg";
 export const Images: Record<string, string> = {
   StarIcon,
 
-  PragaR1,
-  PuritaliaBerlinetta,
-  RaesrTachyonSpeed,
-  RaesrTartarus,
-  RenaultDezir,
-  RenaultRS01,
-  RenaultTrezor,
-  RezvaniBeastX,
-  RimacConceptS,
-  RimacNevera,
-  RimacNeveraTimeAttack,
-  SaleenS1,
-  SaleenS7TwinTurbo,
-  SinR1550,
-  SpaniaGTA2015GTASpano,
-  SSCTuatara,
-  TechrulesAT96TrackVersion,
-  TorinoDesignSuperSport,
-  Toroidion1MW,
-  TrionNemesis,
-  TushekTS900RacerPro,
-  TVRGriffith,
-  TVRSagaris,
-  UltimaRS,
   VencerSarthe,
   Vision1789,
   VLFForce1V10,
