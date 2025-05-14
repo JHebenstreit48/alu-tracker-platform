b0dc548b92608475bfa7a30ac5ce0487763acce6
Justin <j.hebenstreit25@gmail.com>
2025-05-14 16:01:51 -0500

    Initial front and back end repo split

--- client/src/components/Shared/Loading/LoadingContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export interface LoadingContextType {
-  loading: boolean;
-  setLoading: (state: boolean) => void;
-}
-
-export const LoadingContext = createContext<LoadingContextType | null>(null);
