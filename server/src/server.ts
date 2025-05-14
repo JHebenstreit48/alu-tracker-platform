b2a3e3f85c8a540e977f3511d4e93a2c772203e1
Justin <j.hebenstreit25@gmail.com>
2025-05-14 15:31:49 -0500

    removed trailing slash from Netlify link in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://asphaltlegendsunitetracker.netlify.app/"
+    "https://asphaltlegendsunitetracker.netlify.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
