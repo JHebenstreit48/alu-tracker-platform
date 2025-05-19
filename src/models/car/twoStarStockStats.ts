39d2f2ac9779fd9fe067e4af27b5cfe1a8d4aa27
Justin <j.hebenstreit25@gmail.com>
2025-05-19 18:09:26 -0500

    fixed an interface for cars

--- src/models/car/twoStarStockStats.ts
+++ src/models/car/twoStarStockStats.ts
@@ -1,8 +1,8 @@
 export const twoStarStockStats = {
     Two_Star_Stock_Rank: { type: Number },
-    Two_Star_Top_Speed: { type: Number },
-    Two_Star_Acceleration: { type: Number },
-    Two_Star_Handling: { type: Number },
-    Two_Star_Nitro: { type: Number },
+    Two_Star_Stock_Top_Speed: { type: Number },
+    Two_Star_Stock_Acceleration: { type: Number },
+    Two_Star_Stock_Handling: { type: Number },
+    Two_Star_Stock_Nitro: { type: Number },
   };
   
