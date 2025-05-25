dcba2e7a750e0ae4e6ae228eead09ed0eba79ab7
Justin <j.hebenstreit25@gmail.com>
2025-05-25 18:25:46 -0500

    forgot to updated Car.ts modular schema piece

--- src/models/car/Car.ts
+++ src/models/car/Car.ts
@@ -1,8 +1,12 @@
 import { baseCarInfo } from "@/models/car/baseCarInfo";
 import { blueprints } from "@/models/car/blueprints";
 import { stockStats } from "@/models/car/stockStats";
-import { oneStarStockStats } from "@/models/car/oneStarMaxStats";
-import { twoStarStockStats } from "@/models/car/twoStarMaxStats";
+import { oneStarMaxStats } from "@/models/car/oneStarMaxStats";
+import { twoStarMaxStats } from "@/models/car/twoStarMaxStats";
+import { threeStarMaxStats } from "@/models/car/threeStarMaxStats";
+import { fourStarMaxStats } from "@/models/car/fourStarMaxStats";
+import { fiveStarMaxStats } from "@/models/car/fiveStarMaxStats";
+import { sixStarMaxStats } from "@/models/car/sixStarMaxStats";
 import { goldMaxStats } from "@/models/car/goldMaxStats";
 
 // ✅ Utility type to extract all keys as optional fields
@@ -16,6 +20,10 @@ export type Car = {
 } & ExtractKeys<typeof baseCarInfo>
   & ExtractKeys<typeof blueprints>
   & ExtractKeys<typeof stockStats>
-  & ExtractKeys<typeof oneStarStockStats>
-  & ExtractKeys<typeof twoStarStockStats>
+  & ExtractKeys<typeof oneStarMaxStats>
+  & ExtractKeys<typeof twoStarMaxStats>
+  & ExtractKeys<typeof threeStarMaxStats>
+  & ExtractKeys<typeof fourStarMaxStats>
+  & ExtractKeys<typeof fiveStarMaxStats>
+  & ExtractKeys<typeof sixStarMaxStats>
   & ExtractKeys<typeof goldMaxStats>;
