bd9494ced0bd2b93d545e9d17436127ffcaa16f8
Justin <j.hebenstreit25@gmail.com>
2025-05-25 18:21:25 -0500

    added to modular schema star ranks and updated .json

--- src/models/car/schema.ts
+++ src/models/car/schema.ts
@@ -2,17 +2,25 @@ import mongoose, { Schema } from "mongoose";
 import { baseCarInfo } from "@/models/car/baseCarInfo";
 import { blueprints } from "@/models/car/blueprints";
 import { stockStats } from "@/models/car/stockStats";
-import { oneStarStockStats } from "@/models/car/oneStarMaxStats";
-import { twoStarStockStats } from "@/models/car/twoStarMaxStats";
+import { oneStarMaxStats } from "@/models/car/oneStarMaxStats";
+import { twoStarMaxStats } from "@/models/car/twoStarMaxStats";
 import { goldMaxStats } from "@/models/car/goldMaxStats";
+import { threeStarMaxStats } from "./threeStarMaxStats";
+import { fourStarMaxStats } from "./fourStarMaxStats";
+import { fiveStarMaxStats } from "./fiveStarMaxStats";
+import { sixStarMaxStats } from "./sixStarMaxStats";
 
 const carSchemaFields = {
   _id: { type: Schema.Types.ObjectId, auto: true },
   ...baseCarInfo,
   ...blueprints,
   ...stockStats,
-  ...oneStarStockStats,
-  ...twoStarStockStats,
+  ...oneStarMaxStats,
+  ...twoStarMaxStats,
+  ...threeStarMaxStats,
+  ...fourStarMaxStats,
+  ...fiveStarMaxStats,
+  ...sixStarMaxStats,
   ...goldMaxStats
 };
 
