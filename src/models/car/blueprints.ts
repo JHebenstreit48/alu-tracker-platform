f85c538d8d2dffe9eece18c7a0072986e869cd5a
Justin <j.hebenstreit25@gmail.com>
2025-09-27 22:51:25 -0500

    working on blueprints table update

--- src/models/car/blueprints.ts
+++ src/models/car/blueprints.ts
@@ -1,9 +1,11 @@
+import { Schema } from "mongoose";
+
 export const blueprints = {
-  BPs_1_Star: { type: [Number, String] },
-  BPs_2_Star: { type: [Number, String] },
-  BPs_3_Star: { type: [Number, String] },
-  BPs_4_Star: { type: [Number, String] },
-  BPs_5_Star: { type: [Number, String], default: null },
-  BPs_6_Star: { type: [Number, String], default: null },
-  Total_Bps: { type: [Number, String] },
+  BPs_1_Star: { type: Schema.Types.Mixed },
+  BPs_2_Star: { type: Schema.Types.Mixed },
+  BPs_3_Star: { type: Schema.Types.Mixed },
+  BPs_4_Star: { type: Schema.Types.Mixed },
+  BPs_5_Star: { type: Schema.Types.Mixed, default: null },
+  BPs_6_Star: { type: Schema.Types.Mixed, default: null },
+  Total_BPs:  { type: Schema.Types.Mixed },
 };
