00ba5419014a3cc28b710fd428b60687592059b7
Justin <j.hebenstreit25@gmail.com>
2025-09-27 22:24:11 -0500

    updated some schemas and json files

--- src/models/car/blueprints.ts
+++ src/models/car/blueprints.ts
@@ -1,10 +1,9 @@
 export const blueprints = {
-    BPs_1_Star: { type: Number },
-    BPs_2_Star: { type: Number },
-    BPs_3_Star: { type: Number },
-    BPs_4_Star: { type: Number, default: null },
-    BPs_5_Star: { type: Number, default: null },
-    BPs_6_Star: { type: Number, default: null },
-    Total_Bps: { type: Number }
-  };
-  
+  BPs_1_Star: { type: [Number, String] },
+  BPs_2_Star: { type: [Number, String] },
+  BPs_3_Star: { type: [Number, String] },
+  BPs_4_Star: { type: [Number, String] },
+  BPs_5_Star: { type: [Number, String], default: null },
+  BPs_6_Star: { type: [Number, String], default: null },
+  Total_Bps: { type: [Number, String] },
+};
