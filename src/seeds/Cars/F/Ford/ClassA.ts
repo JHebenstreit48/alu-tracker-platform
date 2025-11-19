1cd08a8c00daf48d5e507fae07d5709685573dd0
Justin <j.hebenstreit25@gmail.com>
2025-11-18 23:13:49 -0600

    updated several files and added photos

---
+++ src/seeds/Cars/F/Ford/ClassA.ts
@@ -0,0 +1,5 @@
+import gtFrankieEdition from '@/seeds/Cars/F/Ford/A/gtFrankieEdition.json';
+import teamFordzillaP1 from '@/seeds/Cars/F/Ford/A/teamFordzillaP1.json';
+import shelbySuperSnake from '@/seeds/Cars/F/Ford/A/shelbySuperSnake.json';
+
+export default [gtFrankieEdition, teamFordzillaP1, shelbySuperSnake ];
