936d15f4b389e73ef4ac0201e65f8f322f36e268
Justin <j.hebenstreit25@gmail.com>
2025-11-21 21:25:04 -0600

    updated files

---
+++ src/seeds/Cars/K/Koenigsegg/ClassS.ts
@@ -0,0 +1,10 @@
+import regera from "@/seeds/Cars/K/Koenigsegg/S/regera.json";
+import jesko from "@/seeds/Cars/K/Koenigsegg/S/jesko.json";
+import jeskoAbsolut from "@/seeds/Cars/K/Koenigsegg/S/jeskoAbsolut.json";
+import cc850 from "@/seeds/Cars/K/Koenigsegg/S/cc850.json";
+import gemera from "@/seeds/Cars/K/Koenigsegg/S/gemera.json";
+import ccxr from "@/seeds/Cars/K/Koenigsegg/S/ccxr.json";
+import ageraRS from "@/seeds/Cars/K/Koenigsegg/S/ageraRS.json";
+import oneTo1 from "@/seeds/Cars/K/Koenigsegg/S/oneTo1.json";
+
+export default [ regera, jesko, jeskoAbsolut, cc850, gemera, ccxr, ageraRS, oneTo1 ];
