9619055cdcf821c5ef04be0d1ee00dbbda088699
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-01-08 08:53:50 -0600

    slit up files and updated and created new ones

---
+++ src/seeds/Cars/N/Nissan/ClassD.ts
@@ -0,0 +1,5 @@
+import leafNismoRc from '@/seeds/Cars/N/Nissan/D/leafNismoRc.json';
+import threeSeventyZNismo from '@/seeds/Cars/N/Nissan/D/threeSeventyZNismo.json';
+import threeSeventyZNeonEdition from '@/seeds/Cars/N/Nissan/D/threeSeventyZNeonEdition.json';
+
+export default [leafNismoRc, threeSeventyZNismo, threeSeventyZNeonEdition];
