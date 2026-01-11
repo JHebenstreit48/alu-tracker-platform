f1a473d1f774a4506243c4e1df3cb9515a77114b
J H <j.hebenstreit25@gmail.com>
2026-01-10 22:15:16 -0600

    updated and added several files

---
+++ src/seeds/Cars/R/Raesr/ClassS.ts
@@ -0,0 +1,5 @@
+import tachyonSpeed from '@/seeds/Cars/R/Raesr/S/tachyonSpeed.json';
+import tartarus from '@/seeds/Cars/R/Raesr/S/tartarus.json';
+import aglaia from '@/seeds/Cars/R/Raesr/S/aglaia.json';
+
+export default [tachyonSpeed, tartarus, aglaia];
