8974a7b042c8f047afe9e1012488e82c5284f597
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-01-24 20:52:38 -0600

    updated Apollo cars

---
+++ src/seeds/Cars/A/ApolloCars/ClassB.ts
@@ -0,0 +1,5 @@
+import evo from '@/seeds/Cars/A/ApolloCars/B/evo.json';
+import n from '@/seeds/Cars/A/ApolloCars/B/n.json';
+import ie from '@/seeds/Cars/A/ApolloCars/B/ie.json';
+
+export default [ evo, n, ie ];
