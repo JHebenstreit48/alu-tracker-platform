53581f51468a9dad41a8a01c4183be7005f194f7
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-01 22:12:40 -0600

    updated more files

---
+++ src/seeds/Cars/R/Rimac/ClassS.ts
@@ -0,0 +1,5 @@
+import nevera from '@/seeds/Cars/R/Rimac/S/nevera.json';
+import conceptS from '@/seeds/Cars/R/Rimac/S/conceptS.json';
+import neveraTimeAttack from '@/seeds/Cars/R/Rimac/S/neveraTimeAttack.json';
+
+export default [nevera, conceptS, neveraTimeAttack];
