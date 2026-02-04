adaf3c5c1eb8971822e4b63948375b5ce87a59b3
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-03 18:38:00 -0600

    updated several files and added new files and images

---
+++ src/seeds/Cars/J/Jaguar/ClassB.ts
@@ -0,0 +1,3 @@
+import fTypeSvr from '@/seeds/Cars/J/Jaguar/B/fTypeSvr.json';
+
+export default [fTypeSvr];
---
+++ src/seeds/Cars/J/Jaguar/ClassC.ts
@@ -0,0 +1,5 @@
+import xjr15 from '@/seeds/Cars/J/Jaguar/C/xjr15.json';
+import xeSvProject8 from '@/seeds/Cars/J/Jaguar/C/xeSvProject8.json';
+import xkrSGt from '@/seeds/Cars/J/Jaguar/C/xkrSGt.json';
+
+export default [xjr15, xeSvProject8, xkrSGt];
