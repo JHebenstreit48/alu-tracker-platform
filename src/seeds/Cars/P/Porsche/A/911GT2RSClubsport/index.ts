a4f8fe23f648eb45e051c57e28324c92737d37da
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-07 00:05:14 -0600

    updated more files, especially Porsche in new format

---
+++ src/seeds/Cars/P/Porsche/A/911GT2RSClubsport/index.ts
@@ -0,0 +1,4 @@
+import car from '@/seeds/Cars/P/Porsche/A/911GT2RSClubsport/car.json';
+import stats from '@/seeds/Cars/P/Porsche/A/911GT2RSClubsport/stats.json';
+
+export default {...car, ...stats};
