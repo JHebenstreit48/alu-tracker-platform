4a620fa226cba449626e2d42e352aa9e4490fdfc
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-12 10:44:06 -0600

    prepping for another refactor

---
+++ src/seeds/Cars/L/Lotus/B/Evija/upgrades/index.ts
@@ -0,0 +1,4 @@
+import garageLevelXp from '@/seeds/Cars/L/Lotus/B/ER9/upgrades/garageLevelXp.json';
+import importPartsUpgrades from '@/seeds/Cars/L/Lotus/B/ER9/upgrades/importPartsUpgrades.json';
+
+export default {...garageLevelXp, ...importPartsUpgrades};
