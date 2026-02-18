b944637fd6584016660a690b553433e677185f77
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-18 17:20:38 -0600

    updated Lotus Evija

--- src/seeds/Cars/L/Lotus/B/Evija/upgrades/index.ts
+++ src/seeds/Cars/L/Lotus/B/Evija/upgrades/index.ts
@@ -1,4 +1,4 @@
-import garageLevelXp from '@/seeds/Cars/L/Lotus/B/ER9/upgrades/garageLevelXp.json';
-import importPartsUpgrades from '@/seeds/Cars/L/Lotus/B/ER9/upgrades/importPartsUpgrades.json';
+import garageLevelXp from '@/seeds/Cars/L/Lotus/B/Evija/upgrades/garageLevelXp.json';
+import importPartsUpgrades from '@/seeds/Cars/L/Lotus/B/Evija/upgrades/importPartsUpgrades.json';
 
 export default {...garageLevelXp, ...importPartsUpgrades};
