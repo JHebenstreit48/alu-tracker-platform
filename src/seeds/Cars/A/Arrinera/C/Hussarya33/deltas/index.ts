50fe1ada5b21ebb003c173d44f41102201af1d67
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-20 18:20:21 -0600

    updated Arrinera data

---
+++ src/seeds/Cars/A/Arrinera/C/Hussarya33/deltas/index.ts
@@ -0,0 +1,4 @@
+import importDeltas from '@/seeds/Cars/A/Arrinera/C/Hussarya33/deltas/imports';
+import stageDeltas from '@/seeds/Cars/A/Arrinera/C/Hussarya33/deltas/stages';
+
+export default { ...importDeltas, ...stageDeltas };
---
+++ src/seeds/Cars/A/Arrinera/C/Hussarya33/deltas/stages/index.ts
@@ -0,0 +1,3 @@
+import oneStar from '@/seeds/Cars/W/WMotors/S/LykanNeonEdition/deltas/stages/1star.json';
+
+export default { stageDeltas: { oneStar } };
