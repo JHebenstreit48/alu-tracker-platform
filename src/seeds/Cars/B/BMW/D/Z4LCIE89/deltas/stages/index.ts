430d5969e0b144a14ba733cc1f4175f59975ac76
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-28 20:08:56 -0600

    updated BMW Z4 data

---
+++ src/seeds/Cars/B/BMW/D/Z4LCIE89/deltas/stages/index.ts
@@ -0,0 +1,7 @@
+import oneStar from "@/seeds/Cars/B/BMW/D/Z4LCIE89/deltas/stages/1star.json";
+import twoStar from "@/seeds/Cars/B/BMW/D/Z4LCIE89/deltas/stages/2star.json";
+import threeStar from "@/seeds/Cars/B/BMW/D/Z4LCIE89/deltas/stages/3star.json";
+import fourStar from "@/seeds/Cars/B/BMW/D/Z4LCIE89/deltas/stages/4star.json";
+import fiveStar from "@/seeds/Cars/B/BMW/D/Z4LCIE89/deltas/stages/5star.json";
+
+export default { stageDeltas: { oneStar, twoStar, threeStar, fourStar, fiveStar } };
---
+++ src/seeds/Cars/B/BMW/D/Z4LCIE89/stats/index.ts
@@ -0,0 +1,6 @@
+import stock from'@/seeds/Cars/B/BMW/D/Z4LCIE89/stats/stock.json';
+import stages from '@/seeds/Cars/B/BMW/D/Z4LCIE89/stats/stages';
+import maxStar from '@/seeds/Cars/B/BMW/D/Z4LCIE89/stats/maxStar.json';
+import gold from '@/seeds/Cars/B/BMW/D/Z4LCIE89/stats/gold.json';
+
+export default {...stock, ...stages, maxStar, ...gold};
