25162a8e4751a93ae63b6e86e4eb61f9c246bfc3
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-04-08 17:49:43 -0500

    working on fixing entries

--- src/seeds/cars/D/DeTomaso/A/p72/upgrades/imports/index.ts
+++
@@ -1,5 +0,0 @@
-import costs from "@/seeds/cars/D/DeTomaso/A/P72/upgrades/imports/costs.json";
-import garageLevelXp from "@/seeds/cars/D/DeTomaso/A/P72/upgrades/imports/garageLevelXp.json";
-import requirements from "@/seeds/cars/D/DeTomaso/A/P72/upgrades/imports/requirements.json";
-
-export default { imports: { costs, garageLevelXp, requirements } };
