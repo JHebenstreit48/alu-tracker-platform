28b6a5bc5a197dc7e8ca7e1a5a2e61cd279445dc
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-07-25 20:21:19 -0500

    updated car data

---
+++ src/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/index.ts
@@ -0,0 +1,6 @@
+import car from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/car.json';
+import stats from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/stats';
+import upgrades from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/upgrades';
+import deltas from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/deltas';
+
+export default {...car, ...stats, ...upgrades, ...deltas};
---
+++ src/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/stats/index.ts
@@ -0,0 +1,6 @@
+import stock from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/stats/stock.json';
+import stages from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/stats/stages';
+import maxStar from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/stats/maxStar.json';
+import gold from '@/seeds/cars/A/AstonMartin/S/ValkyrieAMRPro/stats/gold.json';
+
+export default { ...stock, ...stages, maxStar, ...gold };
